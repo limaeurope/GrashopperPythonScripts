3d3aab7405f9f8352fb38e1595cb7bce34367831
samu.karli <samu.karli@limaeurope.hu>
2022-05-18 10:43:33 +0200

    init

--- Remove Bottom Surfaces.py
+++ Remove Bottom Surfaces.py
@@ -4,51 +4,57 @@ __version__ = "2022.04.27"
 hor__ = "samu.karli"
 __version__ = "2022.04.27"
 
-
-if isTest:
-    import ptvsd
-
-    if not ptvsd.is_attached():
-        try:
-            ptvsd.enable_attach(secret = 'dev', address = ('localhost', 2019))
-        except:
-            pass
-
-        ptvsd.wait_for_attach()
-
-#----------------------------------------------------------------------------------
-
-epsilon = 0.01
-
+from ghpythonlib.componentbase import executingcomponent as component
+import Grasshopper, GhPython
+import System
+import Rhino
+import rhinoscriptsyntax as rs
 from Rhino import Geometry
 import sys
 import pprint
 from Grasshopper import DataTree as Tree
 
+class MyComponent(component):
+    def RunScript(self, surf, isTest):
+        print(isTest)
+        if isTest:
+            import ptvsd
 
-def isNotVertical(p_):
-    return not p_.NormalAt(0,0).IsParallelTo(Geometry.Vector3d(0,0,1)) in {1,-1}
+            if not ptvsd.is_attached():
+                try:
+                    ptvsd.enable_attach(secret = 'dev', address = ('localhost', 2019))
+                except:
+                    pass
 
-List = Tree[object]()
+                ptvsd.wait_for_attach()
 
-for i in range(surf.BranchCount):
-    branchList = surf.Branch(i)
-    branchPath = surf.Path(i)
+        #----------------------------------------------------------------------------------
 
-    zBottom = sys.float_info.max
+        epsilon = 0.01
 
-    for brep in branchList:
-        try:
-            item = brep.Surfaces[0]
-            zBottom = min(zBottom, Geometry.AreaMassProperties.Compute(item).Centroid.Z)
-        except (TypeError, AttributeError):
-            continue
+        def isNotVertical(p_):
+            return not p_.NormalAt(0,0).IsParallelTo(Geometry.Vector3d(0,0,1)) in {1,-1}
+
+        List = Tree[object]()
+
+        for i in range(surf.BranchCount):
+            branchList = surf.Branch(i)
+            branchPath = surf.Path(i)
+
+            zBottom = sys.float_info.max
+
+            for brep in branchList:
+                try:
+                    item = brep.Surfaces[0]
+                    zBottom = min(zBottom, Geometry.AreaMassProperties.Compute(item).Centroid.Z)
+                except (TypeError, AttributeError):
+                    continue
     
-    for brep in branchList:
-        try:
-            item = brep.Surfaces[0]
-            if Geometry.AreaMassProperties.Compute(item).Centroid.Z > zBottom + epsilon or isNotVertical(item):
-                List.Add(brep, branchPath)
-        except (TypeError, AttributeError):
-            continue
-
+            for brep in branchList:
+                try:
+                    item = brep.Surfaces[0]
+                    if Geometry.AreaMassProperties.Compute(item).Centroid.Z > zBottom + epsilon or isNotVertical(item):
+                        List.Add(brep, branchPath)
+                except (TypeError, AttributeError):
+                    continue
+        return 1
--- Remove Top Surfaces.py
+++ Remove Top Surfaces.py
@@ -4,51 +4,57 @@ __version__ = "2022.04.27"
 hor__ = "samu.karli"
 __version__ = "2022.04.27"
 
-
-if isTest:
-    import ptvsd
-
-    if not ptvsd.is_attached():
-        try:
-            ptvsd.enable_attach(secret = 'dev', address = ('localhost', 2019))
-        except:
-            pass
-
-        ptvsd.wait_for_attach()
-
-#----------------------------------------------------------------------------------
-
-epsilon = 0.01
-
+from ghpythonlib.componentbase import executingcomponent as component
+import Grasshopper, GhPython
+import System
+import Rhino
+import rhinoscriptsyntax as rs
 from Rhino import Geometry
 import sys
 import pprint
 from Grasshopper import DataTree as Tree
 
-
 def isNotVertical(p_):
     return not p_.NormalAt(0,0).IsParallelTo(Geometry.Vector3d(0,0,1)) in {1,-1}
 
-List = Tree[object]()
+class MyComponent(component):
+    def RunScript(self, surf, isTest):
+        if isTest:
+            import ptvsd
 
-for i in range(surf.BranchCount):
-    branchList = surf.Branch(i)
-    branchPath = surf.Path(i)
+            if not ptvsd.is_attached():
+                try:
+                    ptvsd.enable_attach(secret = 'dev', address = ('localhost', 2019))
+                except:
+                    pass
 
-    zTop = -sys.float_info.max
+                ptvsd.wait_for_attach()
 
-    for brep in branchList:
-        try:
-            item = brep.Surfaces[0]
-            zTop = max(zTop, Geometry.AreaMassProperties.Compute(item).Centroid.Z)
-        except (TypeError, AttributeError):
-            continue
+        #----------------------------------------------------------------------------------
+
+        epsilon = 0.01
+
+        List = Tree[object]()
+
+        for i in range(surf.BranchCount):
+            branchList = surf.Branch(i)
+            branchPath = surf.Path(i)
+
+            zTop = -sys.float_info.max
+
+            for brep in branchList:
+                try:
+                    item = brep.Surfaces[0]
+                    zTop = max(zTop, Geometry.AreaMassProperties.Compute(item).Centroid.Z)
+                except (TypeError, AttributeError):
+                    continue
     
-    for brep in branchList:
-        try:
-            item = brep.Surfaces[0]
-            if Geometry.AreaMassProperties.Compute(item).Centroid.Z < zTop - epsilon or isNotVertical(item):
-                List.Add(brep, branchPath)
-        except (TypeError, AttributeError):
-            continue
+            for brep in branchList:
+                try:
+                    item = brep.Surfaces[0]
+                    if Geometry.AreaMassProperties.Compute(item).Centroid.Z < zTop - epsilon or isNotVertical(item):
+                        List.Add(brep, branchPath)
+                except (TypeError, AttributeError):
+                    continue
+        return List
 
