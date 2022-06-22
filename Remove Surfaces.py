94ec6dcce629960a110b3855138dcfc13940de88
samu.karli <samu.karli@limaeurope.hu>
2022-06-22 16:56:54 +0200

    Remove Surfaces screwed

--- Remove Surfaces.py
+++ Remove Surfaces.py
@@ -2,28 +2,30 @@ __author___ = "samu.karli"
 __version__ = "2022.04.27"
 
 
-if isTest:
-    import ptvsd
+#if isTest:
+#    import ptvsd
 
-    if not ptvsd.is_attached():
-        try:
-            ptvsd.enable_attach(secret = 'dev', address = ('localhost', 2019))
-        except:
-            pass
+#    if not ptvsd.is_attached():
+#        try:
+#            ptvsd.enable_attach(secret = 'dev', address = ('localhost', 2019))
+#        except:
+#            pass
 
-        ptvsd.wait_for_attach(10)
+#        ptvsd.wait_for_attach(10)
 
 #----------------------------------------------------------------------------------
 
 epsilon = 0.01
 
 from Rhino import Geometry
+from Rhino import RhinoMath
 import sys
 import pprint
 from Grasshopper import DataTree as Tree
 
-def isNotVertical(p_):
-    return not p_.NormalAt(0,0).IsParallelTo(Geometry.Vector3d(0,0,1)) in {1,-1}
+def isNotVertical(p_surface, p_aMax):
+    _angle = RhinoMath.ToDegrees(Geometry.Vector3d.VectorAngle(p_surface.NormalAt(0,0), Geometry.Vector3d(0,0,1)))
+    return _angle > p_aMax
 
 List = Tree[object]()
 
@@ -39,6 +41,7 @@ for i in range(surf.BranchCount):
         try:
             item = brep.Surfaces[0]
             zExt = fExt(zExt, Geometry.AreaMassProperties.Compute(item).Centroid.Z)
+            zExt = fExt(zExt, *[p.Z for p in item.Points])
         except (TypeError, AttributeError):
             continue
     
@@ -46,7 +49,8 @@ for i in range(surf.BranchCount):
         try:
             if isinstance(brep, Geometry.Brep):
                 item = brep.Surfaces[0]
-                if fCriterium(Geometry.AreaMassProperties.Compute(item).Centroid.Z) or isNotVertical(item):
+                #if fCriterium(Geometry.AreaMassProperties.Compute(item).Centroid.Z) or isNotVertical(item, aMax):
+                if fCriterium(fExt([p.Z for p in item.Points])) or isNotVertical(item, aMax):
                     List.Add(brep, branchPath)
         except (TypeError, AttributeError):
             continue
