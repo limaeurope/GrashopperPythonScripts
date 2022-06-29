5681a11ffbc9918776779c53fc233d2bf5b5a242
samu.karli <samu.karli@limaeurope.hu>
2022-06-29 17:21:21 +0200

    aMax < _a < 180 - aMax

--- Remove Surfaces.py
+++ Remove Surfaces.py
@@ -2,16 +2,16 @@ __author___ = "samu.karli"
 __version__ = "2022.04.27"
 
 
-if isTest:
-    import ptvsd
+#if isTest:
+#    import ptvsd
 
-    if not ptvsd.is_attached():
-        try:
-            ptvsd.enable_attach(secret = 'dev', address = ('localhost', 2019))
-        except:
-            pass
+#    if not ptvsd.is_attached():
+#        try:
+#            ptvsd.enable_attach(secret = 'dev', address = ('localhost', 2019))
+#        except:
+#            pass
 
-        ptvsd.wait_for_attach(10)
+#        ptvsd.wait_for_attach(10)
 
 #----------------------------------------------------------------------------------
 
@@ -25,9 +25,8 @@ import pprint
 from Grasshopper import DataTree as Tree
 
 def isNotVertical(p_surface):
-    #return not p_.NormalAt(0,0).IsParallelTo(Geometry.Vector3d(0,0,1)) in {1,-1}
     _a = RhinoMath.ToDegrees(Vector3d.VectorAngle(p_surface.NormalAt(0,0), Vector3d(0,0,1)))
-    return _a > aMax
+    return aMax < _a < 180 - aMax
 
 List = Tree[object]()
 
@@ -43,7 +42,6 @@ for i in range(surf.BranchCount):
         try:
             item = brep.Surfaces[0]
             zExt = fExt(zExt, Geometry.AreaMassProperties.Compute(item).Centroid.Z)
-            #zExt = fExt(zExt, *[p.Z for p in item.Points])
         except (TypeError, AttributeError):
             continue
     
@@ -51,8 +49,8 @@ for i in range(surf.BranchCount):
         try:
             if isinstance(brep, Geometry.Brep):
                 item = brep.Surfaces[0]
-                if fCriterium(Geometry.AreaMassProperties.Compute(item).Centroid.Z) or isNotVertical(item):
-                #if fCriterium(fExt([p.Z for p in item.Points])) or isNotVertical(item, aMax):
+                _amp = Geometry.AreaMassProperties.Compute(item)
+                if isNotVertical(item) or fCriterium(_amp.Centroid.Z):
                     List.Add(brep, branchPath)
         except (TypeError, AttributeError):
             continue
